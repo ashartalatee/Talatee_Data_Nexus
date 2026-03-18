2b893f7eeead477ecc368bebca688c6f4e60c938
Ashar <talateeashar@gmail.com>
2026-03-18 21:14:31 +0700

    Add date handling and time feature extraction

--- DataNexus/file_based_engine/02_Sales_Report_Automation_Engine/engine/transformer.py
+++ DataNexus/file_based_engine/02_Sales_Report_Automation_Engine/engine/transformer.py
@@ -1,18 +1,35 @@
 import logging
+import pandas as pd
 
 
 def transform_sales(df):
 
     logging.info("Starting data transformation")
 
-    # pastikan tipe data numeric
+    # pastikan numeric
     df["quantity"] = df["quantity"].astype(float)
     df["price"] = df["price"].astype(float)
 
-    # buat kolom baru: total_sales
+    # total sales
     df["total_sales"] = df["quantity"] * df["price"]
 
     logging.info("Added column: total_sales")
+
+    # =========================
+    # DATE HANDLING
+    # =========================
+
+    # convert ke datetime
+    df["date"] = pd.to_datetime(df["date"], errors="coerce")
+
+    logging.info("Converted date to datetime")
+
+    # extract fitur waktu
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
+    df["day"] = df["date"].dt.day
+
+    logging.info("Extracted year, month, day from date")
 
     logging.info("Data transformation completed")
 
